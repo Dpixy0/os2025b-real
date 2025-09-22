bb21862dff97f4fd4c31ce4182556298bb8353f6
Dpixy0 <dpicxy0@gmail.com>
2025-09-22 15:39:31 +0900

    v0.2 hello/main.go & week05.txt update

--- week05/hello/main.go
+++ week05/hello/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	name := "GO"
-	fmt.Println("Hello", name)
+	// name := "GO"
+	// fmt.Println("Hello", name)
+
+	number := 7
+	fmt.Printf("Number is %d.\n", number)
 }
